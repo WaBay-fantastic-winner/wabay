e899e72c40f8b37ebe47b56b7f8f059d9c3837d9
Mike <wl02599509@gmail.com>
2022-08-18 02:23:56 +0800

    增加 presence validation

--- app/models/donate_item.rb
+++ app/models/donate_item.rb
@@ -1,6 +1,10 @@
 class DonateItem < ApplicationRecord
+  # relationship
   belongs_to :project
 
   has_many :transactions
   has_many :users, through: transactions
+
+  #validation
+  validates :title, :content, :price, presence: true
 end
