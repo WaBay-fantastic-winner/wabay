bca5e571016309f380aa7a5783fd2126c4be79ac
shawn <were1122was@gmail.com>
2022-08-21 20:21:44 +0800

    探索、提案頁面完成

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,9 @@
+class ProjectsController < ApplicationController
+  skip_before_action :authenticate_user!
+  def show
+  end
+  def proposal
+  end
+  def organizers
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
+  resource :projects do
+    collection do
+      get :proposal, :organizers
+    end
+  end
 end
