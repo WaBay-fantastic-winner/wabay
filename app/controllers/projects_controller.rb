daa9a3617036d4cb5af392b541508b397211a393
shawn <were1122was@gmail.com>
2022-08-23 20:57:33 +0800

    提案表單已建好，可寫入資料庫

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,26 @@
 class ProjectsController < ApplicationController
-  skip_before_action :authenticate_user!
-  def show
+  def new
+    @project = Project.new
   end
-  def proposal
+
+  def create
+    
+    # 登入後的使用者，使用多對多方式 .projects ，建立表單
+    @project = current_user.projects.new(clean_params)
+  
+    if @project.save
+       redirect_to projects_path, notic: " 提案成功 !!"
+    else
+       redirect_to root_path
+    end
   end
-  def organizers
+  def index
+    
+  end
+
+  private
+  def clean_params
+    # 資料清洗
+    clean_params = params.require(:project).permit(:organizer, :email, :phone, :project_title,:project_amount_target, :project_end_time, :project_description)
   end
 end
