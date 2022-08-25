53881f8002bd22c9c03c6ced4f53a75016b81789
DazaWoo <darrenwu5106@gmail.com>
2022-08-26 07:48:28 +0800

    setting ENV variable for Email

--- Gemfile
+++ Gemfile
@@ -69,6 +69,7 @@ end
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'devise-i18n', '~> 1.10'
+
 gem 'rails-i18n'
 
 gem 'net-smtp', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" } #production的絕對網址
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: 'darren050106@gmail.com', #你的帳號
+    password: 'iqwpfyrgkzklcltr', #信箱密碼 
+    enable_starttls_auto: true 
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
