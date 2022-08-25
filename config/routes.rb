3eca36eea31d6210ebd51d3fa9a0c64816236546
janewongtw <janewongtw@gmail.com>
2022-08-25 18:20:36 +0800

    建立搜尋路徑

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :projects
+  resources :projects, only: [] do
+    collection do
+      get :search
+    end
+  end
 end
