924eb0a37639fd3f0156bdef5031fc1748a9a19b
Mike <wl02599509@gmail.com>
2022-08-27 01:12:19 +0800

    整理 code

--- app/controllers/donate_items_controller.rb
+++ app/controllers/donate_items_controller.rb
@@ -1,6 +1,6 @@
 class DonateItemsController < ApplicationController
-  # before_action :find_donate_item, only: [:edit, :update, :destroy]
   before_action :current_project, only: %i[index show create update destroy]
+  before_action :find_donate_item, only: %i[show edit update destroy]
 
   # 贊助方案的 index 會顯示在 project 的 show 裡。 一個專案畫面(show)有很多個贊助項目(index)
   def index
@@ -8,7 +8,6 @@ class DonateItemsController < ApplicationController
   end
 
   def show
-    find_donate_item
   end
 
   def new
@@ -21,30 +20,26 @@ class DonateItemsController < ApplicationController
     @donate_item = @current_project.donate_items.new(donate_item_params)
 
     if @donate_item.save
-      redirect_to project_path(id: params[:project_id]), notice: '贊助方案已建立，距離夢想更進一步！'
+      back_to_project_page '贊助方案已建立，距離夢想更進一步！'
     else
       render :new
     end
   end
 
   def edit
-    find_donate_item
   end
 
   def update
-    find_donate_item
-
     if @donate_item.update(donate_item_params)
-      redirect_to project_path(id: params[:project_id]), notice: '贊助方案已編輯。'
+      back_to_project_page '贊助方案已編輯。'
     else
       render :edit
     end
   end
 
   def destroy
-    find_donate_item
     @donate_item.destroy
-    redirect_to project_path(id: params[:project_id]), notice: '贊助方案已刪除。'
+    back_to_project_page '贊助方案已刪除。'
   end
 
   private
@@ -60,5 +55,9 @@ class DonateItemsController < ApplicationController
   def find_donate_item
     current_project
     @donate_item = @current_project.donate_items.find_by!(id: params[:id])
+  end
+
+  def back_to_project_page(notice_sentence)
+    redirect_to project_path(id: params[:project_id]), notice: notice_sentence
   end
 end
