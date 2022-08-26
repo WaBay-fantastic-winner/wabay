2632843be5750b0f57fc8cf57dc811b90e5e9539
Mike <wl02599509@gmail.com>
2022-08-26 16:59:40 +0800

    贊助項目的 content 改成 action_text

--- app/models/donate_item.rb
+++ app/models/donate_item.rb
@@ -4,6 +4,9 @@ class DonateItem < ApplicationRecord
   has_many :transactions
   has_many :users, through: :transactions
 
-  #validation
+  # action_text
+  has_rich_text :content
+
+  # validation
   validates :title, :content, :price, presence: true
 end
