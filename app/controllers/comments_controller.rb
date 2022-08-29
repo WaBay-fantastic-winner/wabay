94d390e4c786cd612311ebed0c89b6a26a325568
shawn <were1122was@gmail.com>
2022-08-29 21:29:42 +0800

    rubocop檢查

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,37 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
-  before_action :authenticate_user!  # 新增、刪除留言都要先有登入
+  before_action :authenticate_user! # 新增、刪除留言都要先有登入
   before_action :find_project, only: [:create]
   before_action :find_comment, only: [:destroy]
   def create
     @comment = @project.comments.new(clean_params) # 從提案的角度來新增留言
     @comment.user = current_user # has_many的關聯
     if @comment.save
-      redirect_to project_path(@project), notice: "留言成功"
+      redirect_to project_path(@project), notice: '留言成功'
     else
-      redirect_to project_path(@project), notice: "留言失敗"
+      redirect_to project_path(@project), notice: '留言失敗'
     end
   end
 
   def destroy
     @comment.destroy
-    redirect_to @comment.project, notice: "留言刪除成功"
+    redirect_to @comment.project, notice: '留言刪除成功'
   end
 
   private
+
   def clean_params
     params.require(:comment).permit(:content)
   end
-  def find_project #找到該提案下方建立留言
+
+  # 找到該提案下方建立留言
+  def find_project
     @project = Project.find_by(params[:article_id])
   end
-  def find_comment #找到該提案下方建立留言
+
+  # 找到該提案下方建立留言
+  def find_comment
     @comment = current_user.comments.find(params[:id])
   end
 end
