88121e96c33f3b56cde5c622e52ba9f423e7d260
shawn <were1122was@gmail.com>
2022-09-01 18:59:30 +0800

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -53,17 +53,6 @@ ActiveRecord::Schema.define(version: 2022_09_01_033013) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "donate_items", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "price"
-    t.integer "count"
-    t.bigint "project_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id"], name: "index_donate_items_on_project_id"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.bigint "project_id", null: false
@@ -73,6 +62,17 @@ ActiveRecord::Schema.define(version: 2022_09_01_033013) do
     t.integer "count", default: 0
     t.index ["project_id"], name: "index_comments_on_project_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "donate_items", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "price"
+    t.integer "count"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_donate_items_on_project_id"
   end
 
   create_table "like_comments", force: :cascade do |t|
@@ -113,9 +113,9 @@ ActiveRecord::Schema.define(version: 2022_09_01_033013) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "donate_items", "projects"
   add_foreign_key "comments", "projects"
   add_foreign_key "comments", "users"
+  add_foreign_key "donate_items", "projects"
   add_foreign_key "like_comments", "comments"
   add_foreign_key "like_comments", "users"
   add_foreign_key "projects", "users"
