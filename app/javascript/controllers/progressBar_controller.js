3592971373428b7c63bedaed31176882e2cc846e
Mike <wl02599509@gmail.com>
2022-09-07 11:41:31 +0800

    Merge remote-tracking branch 'origin/feature/searchPage_no_conflits'

--- app/javascript/controllers/progressBar_controller.js
+++ app/javascript/controllers/progressBar_controller.js
@@ -4,11 +4,8 @@ export default class extends Controller {
     connect() {
     const changeProgress = (progress) => {
         this.element.style.width = `${progress}%`;
-        // this.element.previousSibling.innerHTML = `${progress}%`;
-        // document.querySelector('#progressPercent').innerHTML = `${progress}%`
         this.element.previousSibling.previousSibling.innerHTML = `${progress}%`;
     };
-    /* change progress after 1 second (only for showcase) */
     setTimeout(() => changeProgress(22), 1000);
     setTimeout(() => changeProgress(45), 2000);
     setTimeout(() => changeProgress(85), 4600);
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     '生產階段': 3,
     '產品寄送': 4
   }  
-  const status = '產品寄送'
+  const status = '樣品狀態'
   const completedBullets = bullets.slice(0, bulletClass[status])
   completedBullets.forEach( (ele) => { ele.classList.add('completed') })
   bar.style.width = barClass[status] + "%"
@@ -46,17 +46,5 @@ document.addEventListener('DOMContentLoaded', () => {
       this.classList.toggle('active')
     })
   }
-
-  // -------------left-timer
-  function leftTimer(year, month, day, hour, minute, second) {
-    var leftTime = (new Date(year, month - 1, day, hour, minute, second)) - (new Date());
-    var days = parseInt(leftTime / 1000 / 60 / 60 / 24, 10); 
-    var hours = parseInt(leftTime / 1000 / 60 / 60 % 24, 10); 
-    var minutes = parseInt(leftTime / 1000 / 60 % 60, 10); 
-    var seconds = parseInt(leftTime / 1000 % 60, 10); 
-    document.getElementById("left-time").innerHTML = days + "天" + hours + "時" + minutes + "分" + seconds + "秒";
-  }
-  let timer = setInterval(() => {leftTimer(2022,10,30,10,10,10)}, 1000);
-  console.log(timer);
 })
 
