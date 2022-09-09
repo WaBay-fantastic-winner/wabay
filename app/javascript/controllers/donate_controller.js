7a79e8974b5d6a667c921732783264fef600f241
jie <mrlee112233@yahoo.com.tw>
2022-09-09 17:52:40 +0800

    斗內算錢功能WIP

--- app/javascript/controllers/donate_controller.js
+++ app/javascript/controllers/donate_controller.js
@@ -1,8 +1,12 @@
 import { Controller } from "stimulus"
 
-export default class extends Controller {
-  static targets = [  ]
+let aa = () => {
+  console.log(1222);
+}
 
+export default class extends Controller {
+  static targets = [ 'sum', 'extraDonate' ]
+  
   connect() {
     console.log(123);
   }
@@ -13,18 +17,19 @@ export default class extends Controller {
 
   plus() {
     console.log(33);
+    aa()
   }
 
-  zero() {
-    
+  reset() {
+    this.extraDonateTarget.value = 0
   }
 
   hundred() {
-
+    this.extraDonateTarget.value = Number(this.extraDonateTarget.value) + 100
   }
   
   thousand(){
-
+    this.extraDonateTarget.value = Number(this.extraDonateTarget.value) + 1000
   }
   
   integer(){
