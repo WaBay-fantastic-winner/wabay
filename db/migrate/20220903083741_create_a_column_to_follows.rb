c3b2556ff2716ec6126e3e71edf18b8b6336f18e
Mike <wl02599509@gmail.com>
2022-09-10 12:10:22 +0800

    change migration

--- db/migrate/20220903083741_create_a_column_to_follows.rb
+++
@@ -1,8 +0,0 @@
-class CreateAColumnToFollows < ActiveRecord::Migration[6.1]
-  def change
-    create_table :a_column_to_follows do |t|
-      add_column :follows, :user_id, :integer
-      t.timestamps
-    end
-  end
-end
