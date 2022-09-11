adfcb2973913e052f19a11ea79fd5d3b32eac200
Mike <wl02599509@gmail.com>
2022-09-11 21:04:01 +0800

    設定好 rails / spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,9 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'warden'
+include Warden::Test::Helpers
+include FactoryBot::Syntax::Methods
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
