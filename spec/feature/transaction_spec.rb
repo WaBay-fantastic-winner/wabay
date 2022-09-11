f22e64321425c180d0defa5a04ba04a99f6b583d
Mike <wl02599509@gmail.com>
2022-09-11 22:29:06 +0800

    交易功能測試成功，可操作外網（綠界交易）

--- spec/feature/transaction_spec.rb
+++ spec/feature/transaction_spec.rb
@@ -12,12 +12,29 @@ RSpec.describe "建立交易", type: :feature do
     visit project_donate_item_path(project_id: @donate_item.project_id, id: @donate_item.id)
     click_on '贊助此項目', exact: false
     
-    within 'li.card-num' do
-      fill_in 'input#CCpart1', with: '4311'
-      fill_in 'input#CCpart2', with: '9522'
-      fill_in 'input#CCpart3', with: '2222'
-      fill_in 'input#CCpart4', with: '2222'
+    within 'div.pay-tab-form' do
+      find('input#CCpart1').set('4311')
+      find('input#CCpart2').set('9522')
+      find('input#CCpart3').set('2222')
+      find('input#CCpart4').set('2222')
+
+      find('input#creditMM').set('12')
+      find('input#creditYY').set('24')
+
+      find('input#CreditBackThree').set('222')
+      find('input#CellPhoneCheck').set('0922222222')
     end
-    expect(page).to have_content '金流服務由綠界科技'
+    click_on '立即付款'
+    click_on '關閉 Turn off'
+    click_on '立即付款'
+    click_on '確定 Confirm'
+    
+    click_on '取得OTP服務密碼(Get the password)'
+    within 'div.cvb-input' do
+      find('input#OTP').set('1234')
+    end
+    click_on '送出(Submit)'
+
+    expect(page).to have_content '登入'
   end
 end
