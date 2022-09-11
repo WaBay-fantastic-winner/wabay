439c3e000bebd7bac459642f972777320e8c1c69
jie <mrlee112233@yahoo.com.tw>
2022-09-11 16:15:32 +0800

    donnate_controller 程式優化

--- app/javascript/controllers/donate_controller.js
+++ app/javascript/controllers/donate_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "stimulus"
 
-function sum() {
-      this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value)
-}
 export default class extends Controller {
   static targets = [ 'sum', 'extraDonate' , 'amount', 'formSum' ]
+
+  getSum() {
+    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value)
+    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+  }
   
   connect() {
     this.sumTarget.textContent = this.element.dataset.price;
@@ -12,66 +14,42 @@ export default class extends Controller {
 
   subtract() {
     this.amountTarget.value <= 1 ? this.amountTarget.value = 1 : this.amountTarget.value -- ;
-    console.log(this.context);
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-    
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
 
   plus() {
     this.amountTarget.value ++;
-
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
 
   reset() {
     this.extraDonateTarget.value = 0;
-
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
 
   hundred() {
     this.extraDonateTarget.value = Number(this.extraDonateTarget.value) + 100;
-
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
   
   thousand() {
     this.extraDonateTarget.value = Number(this.extraDonateTarget.value) + 1000;
-
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
   
   hundredsDigit() {
     this.extraDonateTarget.value = Math.ceil(Number(this.extraDonateTarget.value)/100) * 100;
-
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
 
   integer() {
     // 不是正整數或不是數字都會變空字串
     this.amountTarget.value = this.amountTarget.value.replace(/^(0+)|[^\d]+/g,'');
-
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
+    this.getSum()
   }
 
   priceInteger() {
     this.extraDonateTarget.value = this.extraDonateTarget.value.replace(/^(0+)|[^\d]+/g,'');
-    
-    this.sumTarget.textContent = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-
-    this.formSumTarget.value = this.amountTarget.value * this.element.dataset.price + Number(this.extraDonateTarget.value);
-  }
+    this.getSum()
+  }  
 }
