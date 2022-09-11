18e21ff4209b13d1f65e582332661f412ae17c22
Shawn Kuo <were1122was@gmail.com>
2022-09-11 16:41:06 +0800

    完成aws ENV設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_110156) do
+ActiveRecord::Schema.define(version: 2022_09_05_103903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "a_column_to_follows", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -80,15 +75,6 @@ ActiveRecord::Schema.define(version: 2022_09_05_110156) do
     t.index ["project_id"], name: "index_donate_items_on_project_id"
   end
 
-  create_table "follows", force: :cascade do |t|
-    t.boolean "follow"
-    t.integer "followable_id"
-    t.string "followable_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-  end
-
   create_table "like_comments", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "comment_id", null: false
@@ -99,25 +85,13 @@ ActiveRecord::Schema.define(version: 2022_09_05_110156) do
     t.index ["user_id"], name: "index_like_comments_on_user_id"
   end
 
-  create_table "notifications", force: :cascade do |t|
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.string "type", null: false
-    t.jsonb "params"
-    t.datetime "read_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["read_at"], name: "index_notifications_on_read_at"
-    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string "organizer"
     t.string "email"
     t.string "phone"
     t.string "project_title"
     t.integer "project_amount_target"
-    t.string "project_end_time"
+    t.datetime "project_end_time"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
