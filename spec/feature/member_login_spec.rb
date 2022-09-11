9ba6fb13ede5ba0d01998eceab1bb39ac376961f
Mike <wl02599509@gmail.com>
2022-09-11 21:16:25 +0800

    登入測試OK

---
+++ spec/feature/member_login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "member_login", type: :feature do
+  before do
+    @user = create(:user)
+  end
+  
+  it "登入成功" do
+    visit '/'
+    click_on '登入'
+    within "div#sign_in_form" do
+      fill_in '請輸入Email', with: @user.email
+      fill_in '請輸入密碼', with: '33445566'
+    end
+    click_button '登入'
+    expect(page).to have_content "成功登入了。"
+  end
+end
