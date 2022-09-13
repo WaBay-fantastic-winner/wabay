2c1ba04d12bb2f046c3d70ec8e1546e3bd75582b
Darren <darrenwu5106@gmail.com>
2022-09-13 14:03:40 +0800

    fix/remove useless controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
   has_many :projects
 
