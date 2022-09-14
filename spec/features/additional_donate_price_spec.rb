4ecdb0bdbe6f6404bff565a2beb7b17c61fc29dc
Mike <wl02599509@gmail.com>
2022-09-14 14:28:50 +0800

    WIP-額外贊助介面測試

--- spec/features/additional_donate_price_spec.rb
+++ spec/features/additional_donate_price_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.feature "AdditionalDonatePrices", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "額外贊助介面功能", type: :feature do
+  let(:user) { create(:user) }
+  let(:project) { create(:project) }
+  let(:donate_item) { create(:donate_item) }
+
+  before do
+    login_as user
+    visit project_donate_item_path(project_id: donate_item.project_id, id: donate_item)
+  end
+
+  it "數量" do
+    click_on '+'
+    click_on '+'
+    click_on '+'
+    click_on '-'
+
+    amount = find('input#amount').value.to_i
+    sum_from_test = amount * donate_item.price
+
+    expect(page).to have_content(sum_from_test)
+  end
+
+  it "一百、一千" do
+    click_on '100'
+    click_on '1000'
+    click_on '100'
+
+    additional_donate_price = find('input#additional_donate_price').value.to_i
+    sum_from_test = additional_donate_price + donate_item.price
+
+    expect(page).to have_content(sum_from_test)
+  end
+
+  it "自己輸入金額" do
+    find('input#additional_donate_price').set(Faker::Number.number(digits: (4)))
+
+    additional_donate_price = find('input#additional_donate_price').value.to_i
+    sum_from_test = additional_donate_price + donate_item.price
+    # 尚未，在輸入匡輸入數字後，要按下 enter 才能顯示出數字
+    expect(page).to have_content(sum_from_test)
+  end
 end
