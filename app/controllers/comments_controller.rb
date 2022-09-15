bdec5e3b2df91ed5f46c5bdfcf292f0ca7cdeb00
Shawn Kuo <were1122was@gmail.com>
2022-09-15 19:05:06 +0800

    資料清洗改名

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   before_action :find_project, only: %i[create index]
   before_action :find_comment, only: [:destroy]
   def create
-    @comment = @project.comments.new(clean_params) # 從提案的角度來新增留言
+    @comment = @project.comments.new(comment_params) # 從提案的角度來新增留言
     @comment.user = current_user # has_many的關聯
     if @comment.save
       redirect_to project_comments_path(@project), notice: '留言成功'
@@ -21,13 +21,13 @@ class CommentsController < ApplicationController
   def index
     @comment = Comment.new
     # render html: params
-    @comments = @project.comments.where(parent_id: nil).includes(:user).order(id: :desc) # 只抓第一層留言
+    @comments = @project.comments.where(parent_id: nil).order(id: :desc) # 只抓第一層留言
     # @comments = @project.comments.order(id: :desc)
   end
 
   private
 
-  def clean_params
+  def comment_params
     params.require(:comment).permit(:content, :parent_id)
   end
 
