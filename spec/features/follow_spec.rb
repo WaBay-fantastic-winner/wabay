18aa560be98cbfd3cc786f7c3205185a186addf9
Mike <wl02599509@gmail.com>
2022-09-15 23:04:23 +0800

    增加取消追蹤的情境測試。

--- spec/features/follow_spec.rb
+++ spec/features/follow_spec.rb
@@ -17,7 +17,17 @@ RSpec.feature "追蹤專案", type: :feature do
     expect(page).to have_content project.title
   end
 
-  scenario "取消追蹤" do
+  scenario "在專案頁面取消追蹤" do
+    visit project_path(project.id)
+
+    click_on '追蹤專案'
+    sleep 1
+    click_on '取消追蹤'
+
+    expect(page).to have_content '追蹤專案'
+  end
+
+  scenario "在個人追蹤列表內取消追蹤" do
     visit project_path(project.id)
 
     click_on '追蹤專案'
