c708e813a180861bf1633f39bf5df11adc49ac65
LI YU JIE <yujie@LIdeMacBook-Air.local>
2022-09-17 00:53:22 +0800

    優化時間倒數

--- app/javascript/controllers/leftTimer_controller.js
+++ app/javascript/controllers/leftTimer_controller.js
@@ -7,22 +7,15 @@ export default class extends Controller {
   connect() {
     const endTimeArr = this.element.dataset.projectTime.split(' ')
     const endTime = dayjs(endTimeArr[0], endTimeArr[1])
-    const endYear = endTime.$y
-    const endMonth = endTime.$M + 1
-    const endDay = endTime.$D
-    const endHour = endTime.$H
-    const endMinute = endTime.$m
-    const endSecond = endTime.$s
 
-    function leftTimer(year, month, day, hour, minute, second) {
-      const leftTime = (new Date(year, month - 1, day, hour, minute, second)) - (new Date());
-      const days = parseInt(leftTime / 1000 / 60 / 60 / 24, 10);
-      const hours = parseInt(leftTime / 1000 / 60 / 60 % 24, 10);
-      const minutes = parseInt(leftTime / 1000 / 60 % 60, 10); 
-      const seconds = parseInt(leftTime / 1000 % 60, 10); 
-      // this.leftTimeTarget.textContent = days + "天" + hours + "時" + minutes + "分" + seconds + "秒";
+    function leftTimer(endTime) {
+      const totalSeconds = endTime.diff(dayjs(),'seconds')
+      const days = parseInt(totalSeconds / 60 / 60 / 24, 10);
+      const hours = parseInt(totalSeconds / 60 / 60 % 24, 10);
+      const minutes = parseInt(totalSeconds / 60 % 60, 10); 
+      const seconds = parseInt(totalSeconds % 60, 10); 
       document.querySelector('#leftTime').textContent = days + "天" + hours + "時" + minutes + "分" + seconds + "秒";
     }
-    setInterval(function(){leftTimer(endYear,endMonth,endDay,endHour,endMinute,endSecond)}, 1000)
+    setInterval(function(){leftTimer(endTime)}, 1000)
   }
 }
