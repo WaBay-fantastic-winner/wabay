bdb457c5da83565c887dad193c494257beecf7e0
Mike <wl02599509@gmail.com>
2022-09-17 22:07:31 +0800

    把不必要的方法刪除。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :discription])
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username, :discription])
-  # end
 end
