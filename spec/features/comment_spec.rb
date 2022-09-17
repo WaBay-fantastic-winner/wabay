7d992015b202966cfc93c970ee294db3dc6f4d47
Mike <wl02599509@gmail.com>
2022-09-17 15:50:29 +0800

    按讚與刪除留言測試成功。

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -5,6 +5,8 @@ RSpec.feature "在專案底下留言", type: :feature do
   let(:project) { create(:project) }
   let(:content) { 'Wubay好好用' }
   let(:sub_content) { '真的超好用' }
+  let(:comment) { Comment.create(user_id: user.id, project_id: project.id, content: 'Wubay全都馬上就找到工作了') }
+
   it "留言" do
     login_as user
 
@@ -23,10 +25,33 @@ RSpec.feature "在專案底下留言", type: :feature do
     end
 
     find('input.sub-comment-submit').click
+
     expect(page).to have_content('Wubay好好用')
   end
 
   it "按讚" do
+    login_as user
 
+    visit project_path(comment.project_id)
+
+    click_on '留言專區'
+
+    click_on '♡'
+
+    expect(find('span#like-count')).to have_content('1')
+  end
+
+  it "刪除留言" do
+    login_as user
+
+    visit project_path(comment.project_id)
+    
+    click_on '留言專區'
+
+    accept_confirm do
+      click_link '刪除留言'
+    end
+
+    expect(page).not_to have_content(comment.content)
   end
 end
