21f1b3f4d643fc98e6d5a046bde694db1f31d258
Darren <darrenwu5106@gmail.com>
2022-09-17 16:21:50 +0800

    fix/fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callback", registrations: "users/registrations" } 
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations", sessions: "users/sessions" } 
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
     get '/users/profile', to: 'users/registrations#profile'
