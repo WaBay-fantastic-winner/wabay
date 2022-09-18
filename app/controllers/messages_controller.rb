18bce800daa72fac93580a3f9d8cd1e7e975f9d3
Darren <darrenwu5106@gmail.com>
2022-09-18 20:50:46 +0800

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @message = current_user.messages.new(params_message)
+    @message.save!
+    
+
+  end
+
+  private
+
+  def params_message
+    params.require(:message).permit(:content)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_063708) do
+ActiveRecord::Schema.define(version: 2022_09_18_123351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,13 +110,14 @@ ActiveRecord::Schema.define(version: 2022_09_15_063708) do
     t.index ["user_id"], name: "index_like_comments_on_user_id"
   end
 
-  create_table "mail_lists", force: :cascade do |t|
-    t.bigint "user_id", null: false
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id"], name: "index_mail_lists_on_project_id"
-    t.index ["user_id"], name: "index_mail_lists_on_user_id"
+    t.index ["project_id"], name: "index_messages_on_project_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -184,8 +185,8 @@ ActiveRecord::Schema.define(version: 2022_09_15_063708) do
   add_foreign_key "donate_logs", "donate_items"
   add_foreign_key "like_comments", "comments"
   add_foreign_key "like_comments", "users"
-  add_foreign_key "mail_lists", "projects"
-  add_foreign_key "mail_lists", "users"
+  add_foreign_key "messages", "projects"
+  add_foreign_key "messages", "users"
   add_foreign_key "projects", "users"
   add_foreign_key "questions", "projects"
   add_foreign_key "transactions", "donate_items"
