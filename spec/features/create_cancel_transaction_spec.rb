dffe6e2f7e154108bb35e4fb3d7bdcd8cc1b50bb
Mike <wl02599509@gmail.com>
2022-09-20 01:17:59 +0800

    整理測試，並新增專案與贊助的測試。

--- spec/features/create_cancel_transaction_spec.rb
+++ spec/features/create_cancel_transaction_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "交易訂單取消", type: :feature do
+RSpec.feature "交易訂單", type: :feature do
   let(:user) { create(:user) }
   let(:donate_item) { create(:donate_item) }
 
@@ -8,6 +8,11 @@ RSpec.feature "交易訂單取消", type: :feature do
     login_as user
     visit project_donate_item_path(project_id: donate_item.project_id, id: donate_item.id)
     click_on '直接贊助'
+  end
+
+  it "建立交易訂單成功" do
+    visit transactions_path
+    expect(page).to have_content "#{donate_item.title}"
   end
 
   it "取消交易訂單成功" do 
--- spec/features/post_project_spec.rb
+++ spec/features/post_project_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature '建立專案', type: :feature do
-    let(:user) { create(:user) }
+RSpec.feature '專案功能', type: :feature do
+  let(:project) { create(:project) }
+  let(:user) { User.find(project.id) }
 
   it "專案新增成功" do
     login_as user
-    
     visit '/'
     click_button "提案"
 
@@ -16,10 +16,36 @@ RSpec.feature '建立專案', type: :feature do
       fill_in 'project_title', with: Faker::Food.dish
       fill_in 'project_amount_target', with: Faker::Number.number(digits: 4)
       find("input#project_end_time").set("#{Date.new.end_of_year + 1}")
-      # fill_in 'project_end_time', with: Date.new.end_of_year + 1 # 年份有六位數。
-      find("trix-editor").set(Faker::Food.description) # action_text 無法用 fill_in 找到。
+      find("trix-editor").set(Faker::Food.description) 
     end
     click_on '確認送出'
     expect(page).to have_content('提案成功 !!')
+  end
+
+  it "專案編輯成功" do
+    login_as user
+    visit project_path(project.id)
+    click_on '編輯專案'
+
+    within 'div#edit_project' do
+      fill_in 'project_organizer', with: Faker::Name.name
+      fill_in 'project_amount_target', with: Faker::Number.number(digits: 4)
+      find("input#project_end_time").set("#{Date.new.end_of_year + 1}")
+      find("trix-editor").set(Faker::Food.description) 
+    end
+
+    click_on '確認送出'
+    expect(page).to have_content '提案更新成功 !!'
+  end
+
+  it "專案刪除成功" do
+    login_as user
+    visit project_path(project.id)
+
+    accept_confirm do
+      click_on '刪除專案'
+    end
+
+    expect(page).not_to have_content project.title
   end
 end
