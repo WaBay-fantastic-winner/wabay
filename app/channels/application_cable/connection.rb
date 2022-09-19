0985741c3fe047f9958f6eef67131c8cd07e7e4c
Darren <darrenwu5106@gmail.com>
2022-09-19 13:13:53 +0800

    wip/set devise validation in actioncable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -5,17 +5,16 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verfied_user
+      self.current_user = find_verified_user
     end
 
-    protected
-
-    def find_verfied_user
-      if current_user = env['warden'].user
-        current_user
-      else
-        reject_unauthorized_connection
+    private
+      def find_verified_user
+        if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
       end
-    end
   end
 end
