b1edba741f7c3a2ce9ecec4d9e6878554635a220
Mike <wl02599509@gmail.com>
2022-09-19 23:44:48 +0800

    修改 transaction 系列測試流程。

--- spec/features/transaction_cancel_spec.rb
+++ spec/features/transaction_cancel_spec.rb
@@ -8,33 +8,10 @@ RSpec.feature "交易訂單取消", type: :feature do
     login_as user
     visit project_donate_item_path(project_id: donate_item.project_id, id: donate_item.id)
     click_on '直接贊助'
-    
-    within 'div.pay-tab-form' do
-      find('input#CCpart1').set('4311')
-      find('input#CCpart2').set('9522')
-      find('input#CCpart3').set('2222')
-      find('input#CCpart4').set('2222')
-
-      find('input#creditMM').set('12')
-      find('input#creditYY').set('24')
-
-      find('input#CreditBackThree').set('222')
-      find('input#CellPhoneCheck').set('0922222222')
-    end
-    click_on '立即付款'
-    click_on '關閉 Turn off'
-    click_on '立即付款'
-    click_on '確定 Confirm'
-
-    click_on '取得OTP服務密碼(Get the password)'
-
-    within 'div.cvb-input' do
-      find('input#OTP').set('1234')
-    end
-    click_on '送出(Submit)'
   end
 
-  it "取消交易訂單成功" do   
+  it "取消交易訂單成功" do 
+    login_as user
     visit transactions_path
 
     accept_confirm do
--- spec/features/transaction_spec.rb
+++ spec/features/transaction_spec.rb
@@ -3,36 +3,11 @@ require 'rails_helper'
 RSpec.describe "建立交易", type: :feature do
   let(:user) { create(:user) }
   let(:donate_item) { create(:donate_item) }
-  let(:transaction) { create(:transaction) }
 
-  it "交易成功" do
+  it "進入綠界交易頁面" do
     login_as user
     visit project_donate_item_path(project_id: donate_item.project_id, id: donate_item.id)
     click_on '直接贊助'
-    
-    within 'div.pay-tab-form' do
-      find('input#CCpart1').set('4311')
-      find('input#CCpart2').set('9522')
-      find('input#CCpart3').set('2222')
-      find('input#CCpart4').set('2222')
-
-      find('input#creditMM').set('12')
-      find('input#creditYY').set('24')
-
-      find('input#CreditBackThree').set('222')
-      find('input#CellPhoneCheck').set('0922222222')
-    end
-    click_on '立即付款'
-    click_on '關閉 Turn off'
-    click_on '立即付款'
-    click_on '確定 Confirm'
-
-    click_on '取得OTP服務密碼(Get the password)'
-
-    within 'div.cvb-input' do
-      find('input#OTP').set('1234')
-    end
-    click_on '送出(Submit)'
-    expect(transaction).to transition_from(:pending).to(:paid).on_event(:pay)
+    expect(page).to have_content('綠界')
   end
 end
