e1c1b356a58bc68ec1cbc026c85b88fec128fbf9
LI YU JIE <yujie@LIdeMacBook-Air.local>
2022-09-21 17:21:47 +0800

    新增新增title icon

--- app/javascript/controllers/leftTimer_controller.js
+++ app/javascript/controllers/leftTimer_controller.js
@@ -7,13 +7,21 @@ export default class extends Controller {
   connect() {
     const endTimeArr = this.element.dataset.projectTime.split(' ')
     const endTime = dayjs(endTimeArr[0], endTimeArr[1])
+    const leftTime =  document.querySelector('#leftTime')
 
+    let leftTimeID = setInterval(function(){leftTimer(endTime)}, 1000)
     function leftTimer(endTime) {
       const totalSeconds = endTime.diff(dayjs(),'seconds')
-      const leftTime =  document.querySelector('#leftTime')
       if (totalSeconds <= 0) {
         leftTime.textContent = '募款已結束'
         leftTime.classList.add('text-red-500')
+        clearInterval(leftTimeID)
+        document.querySelector('.donate-link').remove()
+        let closeDonate = document.createElement('p')
+        closeDonate.setAttribute('class','py-2 text-center text-gray-500 bg-gray-300 rounded-lg')
+        closeDonate.textContent = '募資已結束'
+        document.querySelector('.donate-box').appendChild(closeDonate)
+        document.querySelector('.donate-items')?.remove()
       } else {
         const days = parseInt(totalSeconds / 60 / 60 / 24, 10);
         const hours = parseInt(totalSeconds / 60 / 60 % 24, 10);
@@ -22,6 +30,5 @@ export default class extends Controller {
         leftTime.textContent = days + "天" + hours + "時" + minutes + "分" + seconds + "秒";
       }
     }
-    setInterval(function(){leftTimer(endTime)}, 1000)
   }
 }
