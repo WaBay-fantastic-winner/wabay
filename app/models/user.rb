eeca2834a918f90898203989542701c5084a94d6
LI YU JIE <yujie@LIdeMacBook-Air.local>
2022-09-23 20:07:08 +0800

    刪除第三方臉書

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   private
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.inf.email).first_or_create do |user|
       user.email = auth.info.email
       user.username = auth.info.name || auth.info.email.split('@').first
       user.avatar_url = auth.info.image
