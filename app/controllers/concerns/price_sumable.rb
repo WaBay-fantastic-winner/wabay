c167c9b9866afa977765fdede363c1e4464458f6
Mike <wl02599509@gmail.com>
2023-04-03 01:03:42 +0800

    refactor price_sumable

--- app/controllers/concerns/price_sumable.rb
+++ app/controllers/concerns/price_sumable.rb
@@ -1,9 +1,13 @@
 module PriceSumable
-  def project_current_total(project_id)
-    @sum_of_project = Project.find(project_id).current_total
+  def project(id)
+    Project.find(id)
   end
-  
-  def percentage_of_currency
-    @percentage = ((@sum_of_project.to_f / @project.amount_target).round(2) * 100).to_i
+
+  def project_current_total(id)
+    project(id).current_total
+  end
+
+  def percentage_of_currency(id)
+    @percentage = ((project_current_total(id).to_f / project(id).amount_target).round(2) * 100).to_i
   end
 end
