ce1faef0f4b12832cb694118e7f07cf135538699
Mike <wl02599509@gmail.com>
2023-04-04 18:45:39 +0800

    refactor member login

--- spec/features/member_login_spec.rb
+++ spec/features/member_login_spec.rb
@@ -1,16 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "會員登入", type: :feature, js: true do
+RSpec.describe "Member login", type: :feature, js: true do
+  subject { page }
+
   let(:user) { create(:user) }
+
+  before do
+    visit new_user_session_path
+  end
   
-  it "登入成功" do
-    visit '/'
-    click_on '登入'
-    within "form#new_user" do
-      fill_in '請輸入Email', with: user.email
-      fill_in '請輸入密碼', with: '33445566'
+  context "when member fill in the sheet" do
+    before do
+      within "form#new_user" do
+        fill_in '請輸入Email', with: user.email
+        fill_in '請輸入密碼', with: '33445566'
+      end
+
+      click_button '登入'
     end
-    click_button '登入'
-    expect(page).to have_content "成功登入了。"
+
+    it { is_expected.to have_content "成功登入了。" }
   end
 end
