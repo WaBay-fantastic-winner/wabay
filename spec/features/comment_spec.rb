3094defc4587452e63f69f63121691aa14e2395c
Mike <wl02599509@gmail.com>
2023-04-04 19:21:11 +0800

    refactor comment_spec

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,48 +1,65 @@
 require 'rails_helper'
 
-RSpec.feature "在專案底下留言", type: :feature, js: true do
-  let(:user) { User.find(project.user_id) }
+RSpec.feature "Comments in specific project", type: :feature, js: true do
+  subject { page }
+
   let(:project) { create(:project) }
-  let(:content) { 'Wubay好好用' }
-  let(:sub_content) { '真的超好用' }
-  let(:comment) { Comment.create(user_id: user.id, project_id: project.id, content: 'Wubay全都馬上就找到工作了') }
+  let(:user) { project.user }
 
-  it "留言" do
+  before do
     login_as user
-    visit project_path(project.id)
-    click_on '留言'
-
-    within 'div#comment-area' do
-      fill_in '請輸入留言', with: content
-    end
-
-    find('input.comment-submit').click
-
-    within 'div#sub-comment-area' do
-      fill_in '回覆', with: sub_content
-    end
-
-    find('input.sub-comment-submit').click
-    expect(page).to have_content('Wubay好好用')
   end
 
-  it "按讚" do
-    login_as user
-    visit project_path(comment.project_id)
-    click_on '留言專區'
-    click_on '♡'
-    expect(find('span#like-count')).to have_content('1')
-  end
+  context "when user leave a comment" do
+    let(:comment) { 'Wubay好好用' }
 
-  it "刪除留言" do
-    login_as user
-    visit project_path(comment.project_id)
-    click_on '留言專區'
-
-    accept_confirm do
-      click_link '刪除留言'
+    before do
+      visit project_path(project.id)
+      click_on '留言專區'
+      within 'div#comment-area' do
+        fill_in '請輸入留言', with: comment
+      end
+  
+      find('input.comment-submit').click
     end
 
-    expect(page).not_to have_content(comment.content)
+    it { is_expected.to have_content(comment) }
+  end
+
+  describe 'after a project has a comment' do
+    let(:specific_comment) { Comment.create(user_id: user.id, project_id: project.id, content: 'Wubay全都馬上就找到工作了') }
+
+    before do
+      visit project_path(specific_comment.project_id)
+      click_on '留言專區'
+    end
+
+    context "when user leave a sub-comment" do
+      let(:sub_comment) { '真的超好用' }
+
+      before do
+        within 'div#sub-comment-area' do
+          fill_in '回覆', with: sub_comment
+        end
+
+        find('input.sub-comment-submit').click
+      end
+
+      it { is_expected.to have_content(sub_comment) }
+    end
+
+    context "when user press ♡" do
+      before { click_on '♡' }
+
+      it { is_expected.to have_css('span#like-count', text: '1') }
+    end
+
+    context 'when user delete a comment' do
+      before do
+        accept_confirm { click_link '刪除留言' }
+      end
+
+      it { is_expected.not_to have_content(specific_comment.content) }
+    end
   end
 end
